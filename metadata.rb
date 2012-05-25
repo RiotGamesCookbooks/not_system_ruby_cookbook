f0e0878cdb683e0287a618681d4620bd55414ddd
Michael Ivey <michael.ivey@riotgames.com>
2012-05-24 17:44:08 -0700

    Version 0.1.1 - require 1.3.1 of rbenv to support global attr

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "michael.ivey@riotgames.com"
 license          "Apache 2.0"
 description      "Installs rbenv and Ruby 1.9.3 as the global"
 
-version          "0.1.0"
-depends          "rbenv"
+version          "0.1.1"
+depends          "rbenv", "~> 1.3.1"
